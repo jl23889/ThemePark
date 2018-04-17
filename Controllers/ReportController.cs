e46b0982b1550a309fa4b02ea678bca5c5eee5a1
beleafs <38118864+beleafs@users.noreply.github.com>
2018-04-17 18:34:58 -0500

    fix average
    fix average

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -25,7 +25,7 @@ namespace ThemePark.Controllers
 
     public class Result
     {
-        public Result(int average, int total, DateTime maxDate, int maxCount)
+        public Result(double average, int total, DateTime maxDate, int maxCount)
         {
             this.average = average;
             this.total = total;
@@ -33,7 +33,7 @@ namespace ThemePark.Controllers
             this.maxCount = maxCount;
         }
 
-        public int average { get; set; }
+        public double average { get; set; }
         public int total { get; set; }
         public DateTime maxDate { get; set; }
         public int maxCount { get; set; }
@@ -61,18 +61,30 @@ namespace ThemePark.Controllers
             DateTime startTime = new DateTime(2010, 1, 1);
             DateTime endTime = new DateTime(2011, 1, 1);
 
+            /*
             TimeSpan difference = endTime - startTime;
             int difference_in_days = difference.Days + 1;
+            */
 
             var park = (from ticket in _context.TicketRideEnters
                         where ticket.DateTime <= endTime && ticket.DateTime >= startTime
                         select new { TicketId = ticket.TicketId, DateTime = ticket.DateTime.Date }).Distinct();
 
+            var park_grp = park.GroupBy(x => x.DateTime.Date)
+                .Select(g => new { Dateinfo = g.Key, Count = g.Count() });
+
+            int park_total = park.Count();
+            double park_average = park_grp.Average(l => l.Count);
+            
+            var park_max = park_grp.OrderByDescending(y => y.Count).First();
+
+            /*
             var park_max = park.GroupBy(x => x.DateTime.Date)
                 .Select(g => new { Dateinfo = g.Key, Count = g.Count() }).OrderByDescending(y => y.Count).First();
 
             int park_total = park.Count();
             int park_average = park_total / difference_in_days;
+            */
 
             DateTime park_max_dateinfo = park_max.Dateinfo;
             int park_max_count = park_max.Count;
@@ -91,19 +103,20 @@ namespace ThemePark.Controllers
             DateTime endTime = new DateTime(2011, 1, 1);
             string rideID = "0000000000000000";
 
-            TimeSpan difference = endTime - startTime;
-            int difference_in_days = difference.Days + 1;
+            //TimeSpan difference = endTime - startTime;
+            //int difference_in_days = difference.Days + 1;
 
             var ride = from ticket in _context.TicketRideEnters
                        where ticket.DateTime <= endTime && ticket.DateTime >= startTime && ticket.RideId == rideID
                        select new { ticket.TicketId, ticket.RideId, ticket.DateTime };
 
-            var ride_max = ride.GroupBy(x => x.DateTime.Date)
-                .Select(g => new { Dateinfo = g.Key, Count = g.Count() }).OrderByDescending(y => y.Count).First();
+            var ride_grp = ride.GroupBy(x => x.DateTime.Date)
+                .Select(g => new { Dateinfo = g.Key, Count = g.Count() });
 
+            var ride_max = ride_grp.OrderByDescending(y => y.Count).First();
 
             int ride_total = ride.Count();
-            int ride_average = ride_total / difference_in_days;
+            double ride_average = ride_grp.Average(l => l.Count);//ride_total / difference_in_days;
 
             DateTime ride_max_dateinfo = ride_max.Dateinfo;
             int ride_max_count = ride_max.Count;

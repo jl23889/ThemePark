7bc91f8fc340d0581e0a4909e94cda028b0e8862
beleafs <38118864+beleafs@users.noreply.github.com>
2018-04-17 22:18:10 -0500

    summaryvisit()
    This method takes two Datetime as parameter, returns the detail info about visits for the whole park and each ride in a json table.
    .../api/report/summaryvisit

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -39,6 +39,24 @@ namespace ThemePark.Controllers
         public int maxCount { get; set; }
     }
 
+    public class SummaryResult
+    {
+        public SummaryResult(string name, double average, int total, DateTime maxDate, int maxCount)
+        {
+            this.name = name;
+            this.average = average;
+            this.total = total;
+            this.maxDate = maxDate;
+            this.maxCount = maxCount;
+        }
+
+        public string name { get; set; }
+        public double average { get; set; }
+        public int total { get; set; }
+        public DateTime maxDate { get; set; }
+        public int maxCount { get; set; }
+    }
+
     [Authorize]
     [Route("api/[controller]")]
     public class ReportController : Controller
@@ -55,7 +73,7 @@ namespace ThemePark.Controllers
                 
         [AllowAnonymous]
         [HttpGet("[action]")]
-        public IActionResult CustomerInPark()//(DateTime startTime, DateTime endTime)
+        public IActionResult ParkVisit()//(DateTime startTime, DateTime endTime)
         {
             //the following two lines are for testing purpose, comment out before passing parameters.
             DateTime startTime = new DateTime(2010, 1, 1);
@@ -92,11 +110,10 @@ namespace ThemePark.Controllers
             Result temp = new Result(park_average, park_total, park_max_dateinfo, park_max_count);
             return Ok(temp);
         }
-        
-
+               
         [AllowAnonymous]
         [HttpGet("[action]")]
-        public IActionResult CustomerInRide()//(DateTime startTime, DateTime endTime, string rideID)
+        public IActionResult RideVisit()//(DateTime startTime, DateTime endTime, string rideID)
         {
             //the following three lines are for testing purpose, comment out before passing parameters.
             DateTime startTime = new DateTime(2010, 1, 1);
@@ -123,6 +140,58 @@ namespace ThemePark.Controllers
 
             Result temp = new Result(ride_average, ride_total, ride_max_dateinfo, ride_max_count);
             return Ok(temp);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("[action]")]
+        public IActionResult SummaryVisit()//(DateTime startTime, DateTime endTime)
+        {
+            List<SummaryResult> ls = new List<SummaryResult>();
+
+            //hardcode start
+            DateTime startTime = new DateTime(2010, 1, 1);
+            DateTime endTime = new DateTime(2011, 1, 1);
+            //hardcode end
+
+            var park = (from ticket in _context.TicketRideEnters
+                        where ticket.DateTime <= endTime && ticket.DateTime >= startTime
+                        select new { TicketId = ticket.TicketId, DateTime = ticket.DateTime.Date }).Distinct();
+            var park_grp = park.GroupBy(x => x.DateTime.Date)
+                .Select(g => new { Dateinfo = g.Key, Count = g.Count() });
+            int park_total = park.Count();
+            double park_average = park_grp.Average(l => l.Count);
+            var park_max = park_grp.OrderByDescending(y => y.Count).First();
+            DateTime park_max_dateinfo = park_max.Dateinfo;
+            int park_max_count = park_max.Count;
+
+            ls.Add(new SummaryResult("Park",park_average, park_total, park_max_dateinfo, park_max_count));
+
+            foreach (var ride in _context.Ride)
+            {
+                string rideID = ride.RideId;
+                string rideName = ride.RideName;
+
+                var ridelist = from ticket in _context.TicketRideEnters
+                           where ticket.DateTime <= endTime && ticket.DateTime >= startTime && ticket.RideId == rideID
+                           select new { ticket.TicketId, ticket.RideId, ticket.DateTime };
+
+                if (ridelist.Count() != 0)
+                {
+                    var ride_grp = ridelist.GroupBy(x => x.DateTime.Date).Select(g => new { Dateinfo = g.Key, Count = g.Count() });
+
+                    var ride_max = ride_grp.OrderByDescending(y => y.Count).First();
+
+                    int ride_total = ridelist.Count();
+                    double ride_average = ride_grp.Average(l => l.Count);//ride_total / difference_in_days;
+
+                    DateTime ride_max_dateinfo = ride_max.Dateinfo;
+                    int ride_max_count = ride_max.Count;
+
+                    ls.Add(new SummaryResult(rideName, ride_average, ride_total, ride_max_dateinfo, ride_max_count));
+                }
+            }
+
+            return Ok(ls);
         }
     }
 }

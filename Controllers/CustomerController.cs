5af5f223a06b148535218c6099d561b9cc6c93c1
beleafs <38118864+beleafs@users.noreply.github.com>
2018-04-21 17:02:44 -0500

    detailvisit()
    detailvisit()

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -69,6 +69,7 @@ namespace ThemePark.Controllers
             return transactions_table.ToList();
         }
 
+        [AllowAnonymous]
         [HttpPost("[action]")]
         public IActionResult CreateNewCustomer([FromBody] Customer customer)
         {
--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -65,6 +65,26 @@ namespace ThemePark.Controllers
         public int ageGroup4_over50 { get; set; }
     }
 
+    public class DetailedResult
+    {
+        public DetailedResult(DateTime date, int count, int ageGroup1, int ageGroup2, int ageGroup3, int ageGroup4)
+        {
+            this.date = date;
+            this.count = count;
+            this.ageGroup1_0_to_18 = ageGroup1;
+            this.ageGroup2_19_to_30 = ageGroup2;
+            this.ageGroup3_31_to_50 = ageGroup3;
+            this.ageGroup4_over50 = ageGroup4;
+        }
+
+        public DateTime date { get; set; } 
+        public int count { get; set; }
+        public int ageGroup1_0_to_18 { get; set; }
+        public int ageGroup2_19_to_30 { get; set; }
+        public int ageGroup3_31_to_50 { get; set; }
+        public int ageGroup4_over50 { get; set; }
+    }
+
     public class TicketSalesOutput
     {
         public TicketSalesOutput(string type, int count, decimal totalSales)
@@ -92,6 +112,7 @@ namespace ThemePark.Controllers
             _logger = logger;
         }
 
+        /*
         [AllowAnonymous]
         [HttpGet("[action]")]
         public IActionResult ParkVisit()//(DateTime startTime, DateTime endTime)
@@ -100,12 +121,12 @@ namespace ThemePark.Controllers
             DateTime startTime = new DateTime(2010, 1, 1);
             DateTime endTime = new DateTime(2011, 1, 1);
 
-            /*
-            TimeSpan difference = endTime - startTime;
-            int difference_in_days = difference.Days + 1;
-            */
+            
+            //TimeSpan difference = endTime - startTime;
+            //int difference_in_days = difference.Days + 1;
+            
 
-            var park = (from ticket in _context.TicketRideEnters
+        var park = (from ticket in _context.TicketRideEnters
                         where ticket.DateTime <= endTime && ticket.DateTime >= startTime
                         select new { TicketId = ticket.TicketId, DateTime = ticket.DateTime.Date }).Distinct();
 
@@ -117,13 +138,9 @@ namespace ThemePark.Controllers
             
             var park_max = park_grp.OrderByDescending(y => y.Count).First();
 
-            /*
-            var park_max = park.GroupBy(x => x.DateTime.Date)
-                .Select(g => new { Dateinfo = g.Key, Count = g.Count() }).OrderByDescending(y => y.Count).First();
-
             int park_total = park.Count();
             int park_average = park_total / difference_in_days;
-            */
+            
 
             DateTime park_max_dateinfo = park_max.Dateinfo;
             int park_max_count = park_max.Count;
@@ -162,6 +179,7 @@ namespace ThemePark.Controllers
             Result temp = new Result(ride_average, ride_total, ride_max_dateinfo, ride_max_count);
             return Ok(temp);
         }
+        */
 
         [AllowAnonymous]
         [HttpGet("[action]")]
@@ -330,7 +348,146 @@ namespace ThemePark.Controllers
             return TicketSales(mondayOfLastWeek, sundayOfLastWeek);
         }
 
+        [AllowAnonymous]
+        [HttpGet("[action]")]
+        public IActionResult DetailedVisitPark(DateTime startTime, DateTime endTime)
+        {
+            //hardcode start
+            //DateTime startTime = new DateTime(2010, 1, 1);
+            //DateTime endTime = new DateTime(2010, 1, 7);
+            //hardcode end
 
+            int ageGroup1 = 0;
+            int ageGroup2 = 0;
+            int ageGroup3 = 0;
+            int ageGroup4 = 0;
+
+            int thisYear = DateTime.Now.Year;
+            List<DetailedResult> ls = new List<DetailedResult>();
+
+            var parklist = (from ticket in _context.TicketRideEnters
+                            where ticket.DateTime <= endTime && ticket.DateTime >= startTime
+                            select new { TicketId = ticket.TicketId, DateTime = ticket.DateTime.Date }).Distinct();
+
+            if (parklist.Count() != 0)
+            {
+                var park_grp = parklist.GroupBy(x => x.DateTime.Date).Select(g => new { Dateinfo = g.Key, Count = g.Count() }).OrderBy(y => y.Dateinfo);
+
+                //age distribution for park
+                foreach (var date_record in park_grp)
+                {
+                    ageGroup1 = 0;
+                    ageGroup2 = 0;
+                    ageGroup3 = 0;
+                    ageGroup4 = 0;
+
+                    var customerAgeList = from p in parklist
+                                          join ttp in _context.TransactionTicketPurchases on p.TicketId equals ttp.TicketId
+                                          join ct in _context.CustomerTransaction on ttp.TransactionId equals ct.TransactionId
+                                          join c in _context.Customer on ct.CustomerId equals c.CustomerId
+                                          where p.DateTime.Date == date_record.Dateinfo
+                                          select new { Age = thisYear - c.DateOfBirth.Value.Year };
+
+                    foreach (var customer in customerAgeList)
+                    {
+                        if (customer.Age < 18)
+                            ageGroup1++;
+                        else if (customer.Age < 30)
+                            ageGroup2++;
+                        else if (customer.Age < 50)
+                            ageGroup3++;
+                        else
+                            ageGroup4++;
+                    }
+
+                    ls.Add(new DetailedResult(date_record.Dateinfo, date_record.Count, ageGroup1, ageGroup2, ageGroup3, ageGroup4));
+                }
+            }
+
+            return Ok(ls);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("[action]")]
+        public IActionResult LastWeekDetailedVisitPark()
+        {
+            //hardcode date for testing, replace before deployment
+            //DateTime today = new DateTime(2018, 4, 18);
+            DateTime today = DateTime.Today;
+
+            DateTime mondayOfLastWeek = today.AddDays(-(int)today.DayOfWeek - 6);
+            DateTime sundayOfLastWeek = today.AddDays(-(int)today.DayOfWeek);
+
+            return DetailedVisitPark(mondayOfLastWeek, sundayOfLastWeek);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("[action]")]
+        public IActionResult DetailedVisitRide(string rideID, DateTime startTime, DateTime endTime)
+        {
+            int ageGroup1 = 0;
+            int ageGroup2 = 0;
+            int ageGroup3 = 0;
+            int ageGroup4 = 0;
+
+            int thisYear = DateTime.Now.Year;
+            List<DetailedResult> ls = new List<DetailedResult>();
+
+            var ridelist = from ticket in _context.TicketRideEnters
+                           where ticket.DateTime <= endTime && ticket.DateTime >= startTime && ticket.RideId == rideID
+                           select new { ticket.TicketId, ticket.DateTime };
+
+            if (ridelist.Count() != 0)
+            {
+                var ride_grp = ridelist.GroupBy(x => x.DateTime.Date).Select(g => new { Dateinfo = g.Key, Count = g.Count() }).OrderBy(y => y.Dateinfo);
+                //
+                foreach (var date_record in ride_grp)
+                {
+                    ageGroup1 = 0;
+                    ageGroup2 = 0;
+                    ageGroup3 = 0;
+                    ageGroup4 = 0;
+
+                    var customerAgeList = from p in ridelist
+                                          join ttp in _context.TransactionTicketPurchases on p.TicketId equals ttp.TicketId
+                                          join ct in _context.CustomerTransaction on ttp.TransactionId equals ct.TransactionId
+                                          join c in _context.Customer on ct.CustomerId equals c.CustomerId
+                                          where p.DateTime.Date == date_record.Dateinfo
+                                          select new { Age = thisYear - c.DateOfBirth.Value.Year };
+
+                    foreach (var customer in customerAgeList)
+                    {
+                        if (customer.Age < 18)
+                            ageGroup1++;
+                        else if (customer.Age < 30)
+                            ageGroup2++;
+                        else if (customer.Age < 50)
+                            ageGroup3++;
+                        else
+                            ageGroup4++;
+                    }
+
+                    ls.Add(new DetailedResult(date_record.Dateinfo, date_record.Count, ageGroup1, ageGroup2, ageGroup3, ageGroup4));
+                }
+            }
+
+            return Ok(ls);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("[action]")]
+        public IActionResult LastWeekDetailedVisitRide()//(string rideID)
+        {
+            //hardcode date for testing, replace before deployment
+            string rideID = "0000000000000000";
+            DateTime today = new DateTime(2018, 4, 18);
+            //DateTime today = DateTime.Today;
+
+            DateTime mondayOfLastWeek = today.AddDays(-(int)today.DayOfWeek - 6);
+            DateTime sundayOfLastWeek = today.AddDays(-(int)today.DayOfWeek);
+
+            return DetailedVisitRide(rideID, mondayOfLastWeek, sundayOfLastWeek);
+        }
     }
 }
 
